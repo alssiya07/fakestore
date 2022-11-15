c21f398aa3e34037d4e5fb3f31222071b0206d74
alssiya <allukukku2000@gmail.com>
2022-11-15 13:04:06 +0530

    nov15

--- store/serializer.py
+++ store/serializer.py
@@ -1,8 +1,23 @@
-from store.models import Products
+from store.models import Products,Carts
 from rest_framework import serializers
 
 class ProductsSerializer(serializers.ModelSerializer):
     id=serializers.CharField(read_only=True)
     class Meta():
         model=Products
-        feilds="__all__"
+        fields="__all__"
+
+class CartSerializer(serializers.ReadOnlyField):
+    product=serializers.CharField(read_only=True)
+    user=serializers.CharField(read_only=True)
+    date=serializers.CharField(read_only=True)
+
+    class Meta:
+        model=Carts
+        fields=["product","user","date"]
+    def create(self,validated_data):
+        user=self.context.get("user")
+        product=self.context.get("products")
+        return Carts.objects.create(
+            **validated_data,user=user,
+            product=product)
--- store/views.py
+++ store/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import permissions,authentication
-from store.serializer import ProductsSerializer
-from store.models import Products
+from rest_framework.decorators import action
+
+from store.serializer import ProductsSerializer,CartSerializer
+from store.models import Products,Carts
 # Create your views here.
 
 class ProductsView(ModelViewSet):
@@ -11,3 +13,33 @@ class ProductsView(ModelViewSet):
     permission_classes=[permissions.IsAuthenticated]
     serializer_class=ProductsSerializer
     queryset=Products.objects.all()
+
+    @action(methods=["GET"],detail=False)
+    def categories(self,request,*args,**kwargs):
+        qs=Products.objects.values_list("category",flat=True).distinct()  
+        # no duplicate values lists
+        return Response(data=qs)
+
+    def list(self,request,*args,**kwargs):
+        qs=Products.objects.all()
+        if "category" in request.query_params:
+            qs=qs.filter(category=request.query_params.get("category"))
+        serializer=ProductsSerializer(qs,many=True)
+        return Response(data=serializer.data)
+
+# localhost:8000/products/1/addto_cart/
+
+    @action(methods=["POST"],detail=True)
+    def addto_cart(self,requst,*args,**kwargs):
+
+        product=self.get_object()
+        user=requst.user
+        serializer=CartSerializer(
+            data=requst.data,
+            context={"user":user,"product":product}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data)
+        else:
+            return Response(data=serializer.errors)
